4ccace2e5ca3692b262314ec18b55099fdc4270e
Clint Stapleton <clintmstapleton102@gmail.com>
2024-12-03 15:09:22 -0600

    Update weatherAppGui.java
    fixed bugs

--- weatherAppGui.java
+++ weatherAppGui.java
@@ -1,6 +1,13 @@
 import javax.swing.JTextField;
 import javax.swing.JFrame;
 import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.Cursor;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class weatherAppGui extends JFrame { //inheritance to the jFrame class gui
     public weatherAppGui () {
@@ -38,5 +45,34 @@ public class weatherAppGui extends JFrame { //inheritance to the jFrame class gu
         searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
 
         add(searchTextField);
+
+        //search button
+        JButton searchButton = new JButton(loadImage("C:/Users/clint/OneDrive/Documents/School/My Major!/Code/Projects/unfinished projects/weatherApp/weather/src/assets/search.png"));
+
+        //change the cursor to a hand cursor when hovering over this button
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
     }
-}
+
+    //method to create image in our gui components
+    private ImageIcon loadImage(String resourcePath) {
+        try {
+
+
+            //read the image file from thepath given
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            //return image icon so that our component renders it
+            return new ImageIcon(image);
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Could not find resource"); //send error message to the console
+        return null;
+    
+        }
+
+    }
+
